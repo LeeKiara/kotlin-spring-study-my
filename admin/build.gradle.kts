8b3e793279069f21d2d8a1c2c3f70b0720139723
LeeHyunMee <hbin1028@gmail.com>
2023-11-07 18:15:36 +0900

    로그 수정

--- admin/build.gradle.kts
+++ admin/build.gradle.kts
@@ -1,21 +1,21 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.1.4"
-	id("io.spring.dependency-management") version "1.1.3"
-	kotlin("jvm") version "1.8.22"
-	kotlin("plugin.spring") version "1.8.22"
+    id("org.springframework.boot") version "3.1.4"
+    id("io.spring.dependency-management") version "1.1.3"
+    kotlin("jvm") version "1.8.22"
+    kotlin("plugin.spring") version "1.8.22"
 }
 
 group = "com.bookshop"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_17
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 extra["springCloudVersion"] = "2022.0.4"
@@ -23,53 +23,57 @@ extra["springCloudVersion"] = "2022.0.4"
 val exposedVersion: String by project
 dependencies {
 
-	//commerce 의존성
-	implementation("org.springframework.boot:spring-boot-starter-amqp")
-	implementation("org.springframework.boot:spring-boot-starter-data-redis")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.amqp:spring-rabbit-test")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	runtimeOnly("com.mysql:mysql-connector-j")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    //commerce 의존성
+    implementation("org.springframework.boot:spring-boot-starter-amqp")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+    implementation("org.projectlombok:lombok:1.18.26")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.amqp:spring-rabbit-test")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    runtimeOnly("com.mysql:mysql-connector-j")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 
-	//추가
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-	implementation("com.auth0:java-jwt:4.4.0")
-	implementation("at.favre.lib:bcrypt:0.10.2")
+    implementation("org.projectlombok:lombok")
 
-	//expose 의존성
-	implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
-	implementation("org.jetbrains.exposed:exposed-crypt:$exposedVersion")
-	implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
-	implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
 
-	implementation("org.jetbrains.exposed:exposed-java-time:$exposedVersion")
+    //추가
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("com.auth0:java-jwt:4.4.0")
+    implementation("at.favre.lib:bcrypt:0.10.2")
 
-	implementation("org.jetbrains.exposed:exposed-json:$exposedVersion")
-	implementation("org.jetbrains.exposed:exposed-money:$exposedVersion")
-	implementation("org.jetbrains.exposed:exposed-spring-boot-starter:$exposedVersion")
+    //expose 의존성
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-crypt:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+
+    implementation("org.jetbrains.exposed:exposed-java-time:$exposedVersion")
+
+    implementation("org.jetbrains.exposed:exposed-json:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-money:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-spring-boot-starter:$exposedVersion")
 
 }
 
 dependencyManagement {
-	imports {
-		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
-	}
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs += "-Xjsr305=strict"
-		jvmTarget = "17"
-	}
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "17"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
--- admin/src/main/kotlin/com/bookshop/admin/payment/PaymentService.kt
+++ admin/src/main/kotlin/com/bookshop/admin/payment/PaymentService.kt
@@ -2,16 +2,11 @@ package com.bookshop.admin.payment
 
 import com.bookshop.admin.order.Orders
 import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.JoinType
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.jetbrains.exposed.sql.update
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestAttribute
-import java.sql.Connection
 
 @Service
 class PaymentService(private val database: Database) {
@@ -19,19 +14,22 @@ class PaymentService(private val database: Database) {
     //에러 로그 확인을 위해
     private val logger = LoggerFactory.getLogger(this.javaClass.name)
 
-    // 무통장 입금여부가 Y로 들어오면 주문완료로 처리한다.
+    // 온라인 입금여부가 Y로 들어오면 주문완료로 처리한다.
     fun updateOrdersStatus(bankDepositList: List<BankDepositResponse>) {
-        println("\n<<< PaymentService 무통장 입금 후 주문완료 처리 >>>")
+        println("\n┌─────────────────────────────────────────────────────────────┐");
+        println("│               온라인 입금으로 결제 건 주문완료 처리.              │");
+        println("├─────────────────────────────────────────────────────────────┤");
+
+
 
         transaction {
             for (depositList in bankDepositList) {
-                println("depositList => orderId: ${depositList.orderId}, 입금여부: ${depositList.deposit}")
 
                 // select * from orders WHERE id = :id
                 val query =
-                    Orders.select {
-                        (Orders.id eq depositList.orderId)
-                    }
+                        Orders.select {
+                            (Orders.id eq depositList.orderId)
+                        }
 
                 if (query.count() > 0) {
                     // 해당 주문건의 무통장 입금여부가 Y로 들어와서 주문완료로 처리한다.
@@ -41,12 +39,17 @@ class PaymentService(private val database: Database) {
                     Orders.update({ Orders.id eq depositList.orderId }) {
                         it[orderStatus] = "1";
                     }
+
+                    println(" │ 주문번호 : ${depositList.orderId}, 주문완료: }")
+
+                } else {
+                    println("├ [ 주문대상 건수 : 0 ]                                         ┤");
+                    println("└─────────────────────────────────────────────────────────────┘");
                 }
             }
         }
 
     }
-
 
 
 }
